eaccd253fd06da0b4a3add7317d6de270e5df306
root <root@raspberrypi.(none)>
2013-07-07 00:22:50 +0200

    made image snapshop more stable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src import auth
 from src import html
-import sys, cgi, json
+import sys, cgi, json, subprocess
 
 form = cgi.FieldStorage()
 try:
@@ -10,7 +10,10 @@ except KeyError, e:
 
 result = auth.verify()
 htmlData = '<h1>Nej du!</h1>'
+proc = subprocess.Popen('fswebcam -d /dev/video0 -r 640x480 /var/www/motion/cam.jpg', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+out, htmlData = proc.communicate()
 if result:
+        #subprocess.call(['/var/www/script/take_picture.sh'])
 	htmlData = html.load('main')
 
 jsonData = {'result': result, 'html': htmlData}
